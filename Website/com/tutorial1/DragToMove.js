1e86271e3aefcba3d07a4e6c7683ad227ea5a05c
DTStego <58103561+DTStego@users.noreply.github.com>
2020-10-06 01:47:22 -0700

    Added separate tutorial1 webpage to test iPad animation not working for navbar

---
+++ Website/com/tutorial1/DragToMove.js
@@ -0,0 +1,33 @@
+const slider = document.querySelector('.timelineWrapper');
+let isClicked = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) =>
+{
+    isClicked = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+})
+
+slider.addEventListener('mouseleave', () =>
+{
+    isClicked = false;
+    slider.classList.remove('active');
+})
+
+slider.addEventListener('mouseup', () =>
+{
+    isClicked = false;
+    slider.classList.remove('active');
+})
+
+slider.addEventListener('mousemove', (e) =>
+{
+    if (!isClicked) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    slider.scrollLeft = scrollLeft - walk;
+})
